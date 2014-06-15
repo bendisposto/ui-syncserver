7cc43387e1aa2cbc3f4d2f32eb150f9eb3d3dbee
Jens Bendisposto <jens@bendisposto.de>
2014-06-15 09:50:03 +0200

    updated java interface

--- java/syncserver/UIState.java
+++ java/syncserver/UIState.java
@@ -4,78 +4,29 @@ import clojure.lang.*;
 import clojure.java.api.Clojure;
 import java.io.StringReader;
 import java.util.concurrent.Callable;
+import java.util.*;
 
 public class UIState {
         
-    private static IFn jmodify, jdelete, jchange, jcommit, conj, jconj, delta;
-
-    public final static PersistentVector EMPTY_VECTOR = (PersistentVector) Clojure.read("[]");
-    private final static IPersistentMap EMPTY_MAP = (IPersistentMap) Clojure.read("{}");
-    private final PersistentVector tx; 
-    public final IPersistentMap state;
+    public static IFn transact, delta;
     
     static {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("syncserver.core"));
-        jmodify = Clojure.var("syncserver.core", "jmodify");
-        jdelete = Clojure.var("syncserver.core", "jdelete");
-        jchange = Clojure.var("syncserver.core", "jchange");
-        jconj = Clojure.var("syncserver.core", "jconj");
-        jcommit = Clojure.var("syncserver.core", "commit");
-        conj = Clojure.var("clojure.core","conj");
+        transact = Clojure.var("syncserver.core", "transact");
         delta = Clojure.var("syncserver.core", "delta");
     }
     
-    public UIState() {
-        this(UIState.EMPTY_MAP,UIState.EMPTY_VECTOR);
-    }
-
-
-    public UIState(IPersistentMap state, PersistentVector tx) {
-        this.tx = tx;
-        this.state = state;
-    }
-
-    public UIState change(Object value, String... path) {
-        PersistentVector tx = (PersistentVector) conj.invoke(this.tx, jchange.invoke(value, path));
-        return new UIState(this.state, tx);
+    public static Object transact(Object state, List tx) {
+        return transact.invoke(state, tx);
     }
     
-    public UIState addToVector(Object value, String... path) {
-        PersistentVector tx = (PersistentVector) conj.invoke(this.tx, jconj.invoke(value, path));
-        return new UIState(this.state, tx);
+    public static void delta(Object t1, Object t2) {
+        delta.invoke(t1,t2); 
     }
 
-    public UIState modify(IFunction1 fn, String... path) {
-        PersistentVector tx = (PersistentVector) conj.invoke(this.tx, jmodify.invoke(fn, path));
-        return new UIState(this.state, tx);
+    public static void delta(Object t2) {
+        delta.invoke(new HashMap(),t2); 
     }
-    
-    public UIState delete(String... path) {
-        PersistentVector tx = (PersistentVector) conj.invoke(this.tx, jdelete.invoke(path));
-        return new UIState(this.state, tx);
-    }
-    
-    public UIState commit() {
-        return new UIState((IPersistentMap)jcommit.invoke(this.state, this.tx), UIState.EMPTY_VECTOR);
-    }
-    
-    public static void delta(UIState t1, UIState t2) {
-        delta.invoke(t1.state,t2.state); 
-    }
-
-    public static void delta(UIState t2) {
-        delta.invoke(EMPTY_MAP,t2.state); 
-    }
-
-    public static void main(String[] args) {
-        UIState t1 = new UIState().change(UIState.EMPTY_VECTOR,"foo","bar").commit();
-        UIState t2 = t1.addToVector(12,"foo","bar")
-                    .addToVector(14,"foo","bar")
-                    .addToVector(13,"foo","bar")
-                    .commit(); 
-        delta(t1,t2);           
-    }
-    
 
 }
