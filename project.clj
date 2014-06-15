18ce778015e3dfa4960dc0b60b993b5748d6561c
Jens Bendisposto <jens@bendisposto.de>
2014-06-15 12:47:53 +0200

    refined v2

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.codehaus.groovy/groovy-all "2.3.0"]]
   :java-source-paths ["java"]
   :aot [syncserver.core]
+  :main syncserver.core
   :repositories [["snapshots" {:url "http://cobra.cs.uni-duesseldorf.de/artifactory/libs-snapshot-local/"
                             :username "leiningen"
                             :password :env}]]  
--- src/syncserver/core.clj
+++ src/syncserver/core.clj
@@ -1,34 +1,45 @@
 (ns syncserver.core
+  (:gen-class)
   (:require [clojure.data :as d])
-  (:import syncserver.IFunction1 syncserver.IFunction2))
+  (:import syncserver.IFunction1 
+           syncserver.IFunction2 
+           groovy.lang.Closure
+           groovy.util.Eval))
 
-
+(defn arg-count [f]
+  (let [m (first (.getDeclaredMethods (class f)))
+        p (.getParameterTypes m)]
+    (alength p)))
 
 (defn delta [os ns]
   (println (d/diff os ns))
   (println "old state:" os)
   (println "new state:" ns))
 
-(defn groovy [s cls]
- (fn [x]
-   (let [n (.getMaximumNumberOfParameters cls)]
-     ; (println x s cls)
-      (case n
+(defn groovy [s x cls]
+      (case (.getMaximumNumberOfParameters cls)
          0 (.call cls)
          1 (.call cls x)
-         2 (.call cls [x s])))))
-
-(defn super-class-names [d]
-  (into #{} (map #(.getName %) (supers (type d)))))
+         2 (.call cls [x s])))
 
 (defn transform [[p e]]
  (let [t (type e)]
  (fn [s] 
    (cond
-      (contains? (super-class-names t) "groovy.lang.Closure") (update-in s p (groovy s e))
-      (fn? e) (update-in s p e)
+      (contains? (supers t) groovy.lang.Closure) 
+         (update-in s p #(groovy s % e))
+      (and (fn? e) (= 1 (arg-count e))) (update-in s p e)
+      (and (fn? e) (= 2 (arg-count e))) (update-in s p (partial e s))
       :else (update-in s p (constantly e))))))
 
 (defn transact [state txs] ((->> txs (map transform) reverse (apply comp)) state))
+
+(defn -main [& args]
+  (let [tt (Eval/me "[[['a'], {->1}], [['c'], true], [['b','c'],{->2}], [['a'],{x->x-2}], [['b','c'],{x,s -> s['a']*x*x}]]")]
+    (println (transact {} tt)))
+
+  (let [tt [[["a"] 1] [["a"] inc] [["b"] 5] [["a"] (fn [s e] (* e (s "b")))]]]
+    (println (transact {} tt))))
+
 
 
