31c3714233f1a520b7d4dd9a45d12b3b1eb02b5f
Jens Bendisposto <jens@bendisposto.de>
2014-06-17 13:02:50 +0200

    removed special case

--- src/syncserver/core.clj
+++ src/syncserver/core.clj
@@ -40,13 +40,11 @@
 
 (defn delta [old-state]
   (let [cs @current-state]
-  (if (= old-state (str (:current cs)))
-   (object-array [(:current cs) #{}])
   (if old-state
     (let [os (.getIfPresent cache! old-state)]
       (when os (debug "Cache hit:" os))
       (object-array [(:current cs) (ddiff [] os (:state cs) #{})]))
-    (object-array [(:current cs) (ddiff [] nil (:state cs) #{})])))))
+    (object-array [(:current cs) (ddiff [] nil (:state cs) #{})]))))
 
 (defn groovy [s x cls]
   (case (.getMaximumNumberOfParameters cls)
